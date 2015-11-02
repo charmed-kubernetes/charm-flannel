d3184d47e6987c5d2f93ded2d28bb90c84ebd21c
Charles Butler <chuck@dasroot.net>
2015-11-02 11:13:41 -0500

    Updates to flannel to set `sdn.available`
    Also adds short circuit logic to bail on attempting to re-start flannel after the unit has its networking data

--- reactive/flannel.py
+++ reactive/flannel.py
@@ -1,3 +1,4 @@
+import os
 from shlex import split
 from subprocess import check_call
 
@@ -6,13 +7,20 @@ from charms.reactive import when
 from charms.reactive import when_not
 
 from charmhelpers.core.hookenv import status_set
-
+from charmhelpers.core import unitdata
 
 # Network Port Map
 # protocol | port | source       | purpose
 # ----------------------------------------------------------------------
 # UDP     | 8285  | worker nodes | Flannel overlay network - UDP Backend.
 # UDP     | 8472  | worker nodes | Flannel overlay network - vxlan backend
+
+
+# Example subnet.env file
+# FLANNEL_NETWORK=10.1.0.0/16
+# FLANNEL_SUBNET=10.1.57.1/24
+# FLANNEL_MTU=1450
+# FLANNEL_IPMASQ=false
 
 @when('docker.ready')
 @when_not('etcd.connected')
@@ -29,3 +37,26 @@ def run_bootstrap_daemons(etcd):
     status_set('maintenance', "Installing Flannel networking")
     cmd = "scripts/bootstrap_docker.sh {}".format(etcd.connection_string())
     check_call(split(cmd))
+    ingest_network_config()
+    set_state('sdn.available')
+
+
+def ingest_network_config():
+    ''' Ingest an ENV file, and parse it for data to be consumed in charm logic. '''
+    db = unitdata.kv()
+
+    if db.get('sdn_subnet') and db.get('sdn_mtu'):
+        # We have values, and are possibly good to go
+        return
+
+    if not os.path.isfile('subnet.env'):
+        status_set('waiting', 'Waiting for flannel to come online')
+
+    with open('subnet.env') as f:
+        flannel_config = f.readlines()
+
+    for f in flannel_config:
+        if "FLANNEL_SUBNET" in f:
+            db.set('sdn_subnet', f.split('=')[-1].strip())
+        if "FLANNEL_MTU" in f:
+            db.set('sdn_mtu', f.split('=')[1].strip())
