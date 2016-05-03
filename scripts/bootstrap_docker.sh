cc1b600448e915e0e00eace312bc070c9b8a6ac4
Charles Butler <chuck@dasroot.net>
2016-05-03 16:43:22 -0400

    Refactor bootstrap-docker.sh
    Make the daemon a bit more resilient to being run multiple times

--- scripts/bootstrap_docker.sh
+++ scripts/bootstrap_docker.sh
@@ -19,39 +19,19 @@ cidr=$(config-get cidr)
 connection_string=$1
 
 
-if [ -f /var/run/docker-bootstrap.pid ]; then
-  echo "Docker bootstrap instance pid found. Doing nothing."
-  exit 0
+if [ ! -f /var/run/docker-bootstrap.pid ]; then
+  docker daemon -H unix:///var/run/docker-bootstrap.sock \
+         -p /var/run/docker-bootstrap.pid \
+         --iptables=false \
+         --ip-masq=false \
+         --bridge=none \
+         --graph=/var/lib/docker-bootstrap 2> /var/log/docker-bootstrap.log 1> /dev/null &
 fi
 
 status-set maintenance "Installing Flannel networking"
 
-
-# Cross Platform and does not survive reboots. :cheers:
-
-docker daemon -H unix:///var/run/docker-bootstrap.sock \
-       -p /var/run/docker-bootstrap.pid \
-       --iptables=false \
-       --ip-masq=false \
-       --bridge=none \
-       --graph=/var/lib/docker-bootstrap 2> /var/log/docker-bootstrap.log 1> /dev/null &
-
 # Give it a second
 sleep 1
-
-# Run ETCD for the coordination
-# docker -H unix:///var/run/docker-bootstrap.sock run \
-#        --net=host \
-#        -d \
-#        gcr.io/google_containers/etcd:2.0.12 \
-#        /usr/local/bin/etcd \
-#        --addr=127.0.0.1:4001 \
-#        --bind-addr=0.0.0.0:4001 \
-#        --data-dir=/var/etcd/data
-
-# This takes longer to come online, sleep long enough to let the unit
-# fully register itself.
-# sleep 5
 
 # Register the flannel VXLan
 
@@ -61,24 +41,29 @@ docker -H unix:///var/run/docker-bootstrap.sock run \
         gcr.io/google_containers/etcd:2.0.12 \
         etcdctl -C "${connection_string}" set /coreos.com/network/config "{ \"Network\": \"${cidr}\", \"Backend\": {\"Type\": \"vxlan\"}}"
 
+
+RUNNING=$(docker -H unix:///var/run/docker-boostrap.sock ps -f name=flannel -q)
 # Run flannel daemon to establish the overlay tunnel
-flannelCID=$(docker -H unix:///var/run/docker-bootstrap.sock run \
-            --restart=always \
-            -d \
-            --net=host\
-            --privileged \
-            -v /dev/net:/dev/net \
-            quay.io/coreos/flannel:0.5.3 /opt/bin/flanneld -iface="${interface}" -etcd-endpoints="${connection_string}")
+if [[ "${RUNNING}" ]]; then
+  flannelCID=$(docker -H unix:///var/run/docker-bootstrap.sock run \
+              --restart=always \
+              -d \
+              --net=host\
+              --privileged \
+              -v /dev/net:/dev/net \
+              --name=flannel
+              quay.io/coreos/flannel:latest /opt/bin/flanneld -iface="${interface}" -etcd-endpoints="${connection_string}")
 
-sleep 6
+  sleep 6
 
-# Copy flannel env out and source it on the host
-docker -H unix:///var/run/docker-bootstrap.sock cp ${flannelCID}:/run/flannel/subnet.env .
-source subnet.env
+  # Copy flannel env out and source it on the host
+  docker -H unix:///var/run/docker-bootstrap.sock cp ${flannelCID}:/run/flannel/subnet.env .
+  source subnet.env
 
-# finalize flannel setup
-# TODO: move this to the charm code
-DOCKER_CONF="/etc/default/docker"
-echo "DOCKER_OPTS=\"\$DOCKER_OPTS --mtu=${FLANNEL_MTU} --bip=${FLANNEL_SUBNET}\"" | sudo tee -a ${DOCKER_CONF}
-ifconfig docker0 down
+  # finalize flannel setup
+  # TODO: move this to the charm code
+  DOCKER_CONF="/etc/default/docker"
+  echo "DOCKER_OPTS=\"\$DOCKER_OPTS --mtu=${FLANNEL_MTU} --bip=${FLANNEL_SUBNET}\"" | sudo tee -a ${DOCKER_CONF}
+  ifconfig docker0 down
 apt-get install -y bridge-utils && brctl delbr docker0 && service docker restart
+fi
