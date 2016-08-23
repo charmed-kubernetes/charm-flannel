charmed-kubernetes/charm-flannel

commit 4623e3610a2c308893a67bc3cfd8b59e1ee34480

Adds immutable config fix for bootstrap-daemon

reactive/flannel.py
@@ -177,7 +177,8 @@ def reconfigure_docker_for_sdn():
 
 
 @when_any('config.cidr.changed', 'config.etcd_image.changed',
-          'config.flannel_image.changed', 'config.iface.changed')
+          'config.flannel_image.changed', 'config.iface.changed',
+          'config.http_proxy.changed', 'config.https_proxy.changed')
 def reconfigure_flannel_network():
     ''' When the user changes the cidr, we need to reconfigure the
     backing etcd_store, and re-launch the flannel docker container.'''

commit c83d063ab585797e8dd155fc5ccc5721c21a0830

Patching a bug introduced in 4623e36
- The workload docker daemon was getting consistently reset. This is
  problematic, as it interrupts the workloads running. An unintended
  side effect.

The charm should now only re-render the template, and re-load the
systctl definition, along with recycling the bootstrap-docker daemon.
This has less impact on running workloads than interrupting the primary
workload bridge.

reactive/flannel.py
@@ -61,7 +61,7 @@ def deploy_docker_bootstrap_daemon():
     # future modification. This doesn't add much overhead.
     if codename == 'trusty':
         render('bootstrap-docker.upstart', '/etc/init/bootstrap-docker.conf',
-               {}, owner='root', group='root')
+               config(), owner='root', group='root')
     else:
         # Render the service definition
         render('bootstrap-docker.service',
@@ -176,9 +176,42 @@ def reconfigure_docker_for_sdn():
     set_state('flannel.bridge.configured')
 
 
+@when_any('config.http_proxy.changed', 'config.https_proxy.changed')
+def rerender_service_template():
+    ''' If we change proxy settings, re-render the bootstrap service definition
+    and attempt to resume where we left off.  '''
+
+    # Note: At this point if we hijack the workload daemon, heavy fisted
+    # reprocussions will occur, like disruption  of services.
+
+    codename = host.lsb_release()['DISTRIB_CODENAME']
+    # by default, dont reboot the daemon unless we have previously rendered
+    # system files.
+
+    # Deterministic method to probe if we actually need to restart the
+    # daemon.
+    reboot = (os.path.exists('/lib/systemd/system/bootstrap-docker.service') or
+              os.path.exists('/etc/init/bootstrap-docker.conf'))
+
+    if codename != "trusty":
+        # Handle SystemD
+        render('bootstrap-docker.service',
+               '/lib/systemd/system/bootstrap-docker.service',
+               config(), owner='root', group='root')
+        cmd = ["systemctl", "daemon-reload"]
+        check_call(cmd)
+    else:
+        # Handle Upstart
+        render('bootstrap-docker.upstart',
+               '/etc/init/bootstrap-docker.conf',
+               config(), owner='root', group='root')
+
+    if reboot:
+        service_restart('bootstrap-docker')
+
+
 @when_any('config.cidr.changed', 'config.etcd_image.changed',
-          'config.flannel_image.changed', 'config.iface.changed',
-          'config.http_proxy.changed', 'config.https_proxy.changed')
+          'config.flannel_image.changed', 'config.iface.changed')
 def reconfigure_flannel_network():
     ''' When the user changes the cidr, we need to reconfigure the
     backing etcd_store, and re-launch the flannel docker container.'''