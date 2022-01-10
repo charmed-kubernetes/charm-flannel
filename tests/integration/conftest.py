bf9c4e1d556c0e1b2e07654a7d09b89593ca33e4
Robert Gildein <robert.gildein@canonical.com>
2022-01-10 21:18:02 +0100

    Fix functional tests (#75)
    * Change master branch to main for actions-operator
    
    fixes: https://bugs.launchpad.net/charm-flannel/+bug/1952230
    
    * Using channel candidate for docker-snap, due #45
    
    * switch to charmhub charm names
    
    * install python-libjuju from github due #568
    
    * use charmhub in bundle + remove tmp configuration
    
    * remove tmp solutions for libjuju issue #568 #223
    
    * change to 1.23/edge channel
    
    * add new method to restart kubernetes-worker unit

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,9 +1,10 @@
 from pathlib import Path
+from typing import Tuple
 
 import pytest
 
 
-def pytest_addoption(parser):
+def pytest_addoption(parser) -> None:
     parser.addoption(
         "--flannel-version", nargs="?", type=str, default="amd64",
         choices=["amd64", "arm64", "s390x"],
@@ -11,18 +12,18 @@ def pytest_addoption(parser):
     )
     parser.addoption(
         "--flannel-resource", nargs="?", type=Path,
-        default=Path(__file__).parent.resolve()/".."/".."/"flannel-amd64.tar.gz",
+        default=Path("flannel-amd64.tar.gz"),
         help="The path to the flannel resource. It can be compiled with "
              "`./build-flannel-resources.sh`, see README.md for more information."
     )
 
 
 @pytest.fixture()
-def flannel_resource(pytestconfig):
+def flannel_resource(pytestconfig) -> Tuple[str, str]:
     version = pytestconfig.getoption("--flannel-version")
     path = pytestconfig.getoption("--flannel-resource")
     if not path.exists():
         raise FileNotFoundError("Missing resource, please provide via"
                                 "--flannel-resource option or at {}".format(path))
 
-    return f"flannel-{version}={path}"  # noqa: E999
+    return f"flannel-{version}",  path  # noqa: E999
