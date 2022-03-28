1d18603292b88b659cc666ad0f876bb77c94f02b
Adam Dyess <adam.dyess@canonical.com>
2022-03-28 12:49:26 -0500

    inclusive naming check and black checks (#78)
    * inclusive naming check and black checks
    
    * ignore black testing in py35
    
    * conditional run of tox linting on py35

--- lib/charms/flannel/common.py
+++ lib/charms/flannel/common.py
@@ -2,7 +2,7 @@ from time import sleep
 
 
 def retry(times, delay_secs):
-    """ Decorator for retrying a method call.
+    """Decorator for retrying a method call.
     Args:
         times: How many times should we retry before giving up
         delay_secs: Delay in secs
@@ -10,11 +10,12 @@ def retry(times, delay_secs):
     """
 
     def retry_decorator(func):
-        """ Decorator to wrap the function provided.
+        """Decorator to wrap the function provided.
         Args:
             func: Provided function should return either True od False
         Returns: A callable that would return the last call outcome
         """
+
         def _wrapped(*args, **kwargs):
             res = func(*args, **kwargs)
             attempt = 0
@@ -25,6 +26,7 @@ def retry(times, delay_secs):
                     break
                 attempt += 1
             return res
+
         return _wrapped
 
     return retry_decorator
