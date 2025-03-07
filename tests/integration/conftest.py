d9db936dd984980a2dc8517213ac504da797c5ff
Adam Dyess <adam.dyess@canonical.com>
2025-03-07 14:05:30 -0600

    Drop using snap_channel

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -8,22 +8,11 @@ def pytest_addoption(parser):
     parser.addoption(
         "--series",
         type=str,
-        default="focal",
+        default="jammy",
         help="Set series for the machine units",
-    )
-    parser.addoption(
-        "--snap-channel",
-        type=str,
-        default="1.32/stable",
-        help="Set snap channel for the control-plane & worker units",
     )
 
 
 @pytest.fixture()
 def series(request):
     return request.config.getoption("--series")
-
-
-@pytest.fixture()
-def snap_channel(request):
-    return request.config.getoption("--snap-channel")
